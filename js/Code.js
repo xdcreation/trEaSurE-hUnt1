5507998dedd944ba4495783381956a4f320c2475
xdcreation <69704397+xdcreation@users.noreply.github.com>
2021-06-16 19:45:47 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+
+    fill("white");
+    textSize(15);
+    text("C U N F T O N I", 700,170);
+    fill("lightblue");
+    text("Hint: Performs a particular task", 700,190);
+    
+
+
+    fill("white");
+    textSize(15);
+    text("A T D A S A B E",100,330);
+    fill("lightblue");
+    text("Hint: Stores all information", 100,350);
+
+}
---
+++ js/System.js
@@ -0,0 +1,42 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        
+        if(actualCode === enteredCode.toUpperCase())
+
+        
+    return true
+else
+    return false
+        
+
+
+        if(score === 3) {
+            clear()
+            background(bg2)
+            fill("black")
+            textSize(40);
+            text("TREASURE UNLOCKED",250, 200);
+          }
+
+        
+    }
+    
+
+}
+
+
+        
+    
+
+
+
+
+    
+    
+
